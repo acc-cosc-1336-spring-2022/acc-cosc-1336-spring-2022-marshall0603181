1ce7f001abc5d623e6d778ac508a9933b3b9ed75
marshallb <marshall.brown2@g.austincc.edu>
2022-02-09 20:17:28 -0600

    homework 1

--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -13,7 +13,10 @@ from src.homework.b_in_proc_out.output import multiply_numbers
 class Test_Config(unittest.TestCase):
 
     def test_multiply_numbers_1(self):
-        self.assertEqual(25, 5 , 5)
+        self.assertEqual(25, multiply_numbers(5 , 5))
 
-    def test_multiply_numbers_1(self):
+    def test_multiply_numbers_2(self):
         self.assertEqual(100, multiply_numbers(10 , 10))
+
+    def test_multiply_number_3(self):
+        self.assertEqual(36, multiply_numbers(6,6))
