01db7bd0c2068fa82291dc19388483303fd87112
marshallb <marshall.brown2@g.austincc.edu>
2022-02-15 16:52:58 -0600

    progress

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,12 +1,17 @@
+
+from webbrowser import get
+
+
 num1 = int(input())
 num2 = int(input())
 
 def get_options_ratio(num1, num2):
     return num1 + num2
 
-x = get_options_ratio(num1, num2)
+x = int(get_options_ratio(num1, num2))
 
-def get_faculty_rating(x):
+
+def get_faculty_rating():
     return x
 if 0.9 > x > 1:
     print('Excellent')
