df77ac89a7577326ae88d23abff5b89094e52a45
marshallb <marshall.brown2@g.austincc.edu>
2022-02-16 04:12:19 -0600

    test case additions

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -6,14 +6,18 @@ class Test_Config(unittest.TestCase):
     def test_get_options_ratio(self):
         self.assertEqual(0.25, 5 / 20)
         self.assertEqual(0.5, 10 / 20)
-
-        if get_options_ratio == .91:
+    
+    def test_get_faculty_rating(self):
+        self.assertEqual(1, 6.0 / 6.0)
+        self.assertEqual(0.75, 3 / 4)
+        
+        if get_faculty_rating == .91:
             return 'Excellent'
-        if get_options_ratio == .85:
+        if get_faculty_rating == .85:
             return 'Very Good'
-        if get_options_ratio == .71:
+        if get_faculty_rating == .71:
             return 'Good'
-        if get_options_ratio == .66:
+        if get_faculty_rating == .66:
             return 'Needs Improvement'
-        if get_options_ratio == .45:
+        if get_faculty_rating == .45:
             return 'Unacceptable'
