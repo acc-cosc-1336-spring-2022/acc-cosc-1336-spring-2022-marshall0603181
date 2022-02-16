874460d9737e8972ba4b67d6a83a972c4c2f1221
marshallb <marshall.brown2@g.austincc.edu>
2022-02-16 13:47:16 -0600

    fixed if statements

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -14,11 +14,12 @@ def get_faculty_rating(x):
     return x
 if x >= 0.9 or x == 1:
     print('Excellent')
-if x >= 0.8 and x < 0.9:
+elif x >= 0.8 and x < 0.9:
     print('Very Good')
-if x >= 0.7 and x < 0.8:
+elif x >= 0.7 and x < 0.8:
     print('Good')
 elif x >= 0.6 and x < 0.7:
     print('Needs Improvement')
-if 0 > x <= 0.59 and x < 0.6:
+else: 
+    0 > x <= 0.59 and x < 0.6
     print('Unacceptable')
