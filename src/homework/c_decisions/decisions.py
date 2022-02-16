8303868f68e13bd852acaf998cec1702c75cec39
marshallb <marshall.brown2@g.austincc.edu>
2022-02-16 03:59:57 -0600

    finally understood the instructions

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,24 +1,24 @@
 
 from webbrowser import get
 
-num1 = float(input())
-num2 = float(input())
+option = float(input("input 'option' value:"))
+total = float(input("input 'total' value: "))
 
-def get_options_ratio(num1, num2):
-    return num1 + num2
+def get_options_ratio(option, total):
+    return option / total
 
-x = float(get_options_ratio(num1, num2))
+x = get_options_ratio(option, total)
 
 
-def get_faculty_rating():
+def get_faculty_rating(x):
     return x
 if x >= 0.9 or x == 1:
     print('Excellent')
-if x >= 0.8:
+if x >= 0.8 and x < 0.9:
     print('Very Good')
-if x >= 0.7:
+if x >= 0.7 and x < 0.8:
     print('Good')
-if x >= 0.6:
+elif x >= 0.6 and x < 0.7:
     print('Needs Improvement')
-if 0 >= x <= 0.59:
+if 0 > x <= 0.59 and x < 0.6:
     print('Unacceptable')
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,3 +1,9 @@
 import decisions
+from src.homework.c_decisions.decisions import get_faculty_rating
+
 options = int(input())
 total = int(input())
+
+result = decisions.get_options_ratio(options, total)
+
+print(get_faculty_rating(result))
