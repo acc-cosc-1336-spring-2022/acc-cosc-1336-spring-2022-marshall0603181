e66132c3ec8d2fa563081241bd0f228d7264d843
marshallb <marshall.brown2@g.austincc.edu>
2022-02-17 21:07:47 -0600

    clean up

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,10 +1,4 @@
 import decisions
-#from decisions import get_faculty_rating
-#from decisions import get_options_ratio
 
-options = float(input("input 'options' value: "))
-total = float(input("input 'total' value: "))
-
-result = decisions.get_options_ratio(options, total)
-
-print(result)
+x = decisions.get_faculty_rating
+print(x)
