3e20fcc904b34f21277e51070d12ac807a4b6a33
marshallb <marshall.brown2@g.austincc.edu>
2022-02-19 17:57:57 -0600

    intial commit

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,7 @@ import unittest
 #the file in /tests/homework/c_decisions/tests_decisions.py
 #has the test functions
 
-from tests.homework.c_decisions import tests_decisions
+from tests.homework.d_repetition import tests_repetition
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_decisions)
+suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,16 @@
-#
+#import math
+num = int(input('enter a number: '))
+#factorial = 1
+def get_factorial(num):
+    factorial = 1
+    for i in range(1, num+1):
+        factorial = factorial * i
+        print('the factorial of', num, 'is', factorial)
+get_factorial(num)
+# it works but in a weird way
+num = int(input('enter number: '))
+def sum_odd_numbers(num):
+    while num != 0:
+        print(num)
+        num = 0
+sum_odd_numbers(num)
