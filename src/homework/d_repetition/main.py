11a0cca922ef162ee6acbefe79956d05649941c1
marshallb <marshall.brown2@g.austincc.edu>
2022-02-19 19:26:17 -0600

    added hw 3 menu

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,12 @@
-#
+from repetition import get_factorial
+from repetition import sum_odd_numbers
+print('Homework 3 menu')
+choice = int(input('1-Factorial 2-Sum odd numbers 3-Exit: '))
+if choice == 1:
+    num = int(input('enter number: '))
+    get_factorial(num)
+elif choice == 2:
+    num = int(input('enter number: '))
+    sum_odd_numbers(num)
+elif choice == 3:
+    exit()
