0056afcc4752e4bb219c0b1c92613d27ded42b86
marshallb <marshall.brown2@g.austincc.edu>
2022-02-19 23:15:02 -0600

    fixed the for loop

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,14 +1,16 @@
 num = int(input('enter a number: '))
+
 def get_factorial(num):
     factorial = 1
-    for i in range(1, num+1):
-        factorial = factorial * i
-        print('the factorial of', num, 'is', factorial)
+    if int(num) >= 1:
+        for i in range(1, num+1):
+            factorial = factorial * i
+    print('the factorial of', num, 'is', factorial)
 get_factorial(num)
-# it works but in a weird way
-num = int(input('enter number: '))
-def sum_odd_numbers(num):
-    while num != 0:
-        print(num)
-        num = 0
-sum_odd_numbers(num)
+# it works!
+#num = int(input('enter number: '))
+#def sum_odd_numbers(num):
+#    while num != 0:
+#        print(num)
+#        num = 0
+#sum_odd_numbers(num)
