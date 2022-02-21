43f684eeadc8b6506d393ec4cc407d0c82bf4b45
marshallb <marshall.brown2@g.austincc.edu>
2022-02-20 22:41:51 -0600

    commit

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,17 +1,17 @@
 print('Homework 3 Menu')
 choice = int(input('(1) Factorial (2) Sum odd numbers (3) Exit: '))
 import repetition
-from repetition import num
 # choice has to do with 'Homework 3 menu'.
-if choice == 1:
-    if num > 0 and num < 10:
-        repetition.get_factorial(int(num))
-else:
-    print('Error')
-if choice == 2:
-    repetition.sum_odd_numbers(int(num))
+if choice == 1 and num > 0 and num < 10:
+    from repetition import num
+    repetition.get_factorial(num)
+elif choice == 2:
+    from repetition import num
+    repetition.sum_odd_numbers(num)
 elif choice == 3:
     exit('Exiting...')
+else:
+    print('Error')
 
 # choice2 has to do with the question 'Do you want to exit? y or n: '.
 choice2 = str(input('Do you wish to continue? y or n: '))
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,16 +1,14 @@
 num = int(input('enter a number: '))
-
 def get_factorial(num):
+    num = int(input('enter a number: '))
     factorial = 1
     if int(num) >= 1:
         for i in range(1, num+1):
             factorial = factorial * i
-            fact = "The factorial of {} is {}"
-    print(fact.format(num, factorial))
+    print('The factorial of ', num, 'is', factorial)
 # it works!
 
 def sum_odd_numbers(num):
-    while num != 0:
-        print(num + num - 1)
-        num = 0
-    sum_odd_numbers(int(num))
+    num = int(input('enter a number: '))
+    print('The sum of all the odd numbers before ', num, 'is'\
+        , sum_odd_numbers(num))
