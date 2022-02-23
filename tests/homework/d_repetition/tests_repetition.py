5a0c4abbdf40de92e22d007fdffadb5f1d523b2d
marshallb <marshall.brown2@g.austincc.edu>
2022-02-22 20:14:17 -0600

    .

--- tests/homework/d_repetition/tests_repetition.py
+++ tests/homework/d_repetition/tests_repetition.py
@@ -1,6 +1,5 @@
 import unittest
-import math
-from src.homework.d_repetition.repetition import get_factorial
+from src.homework.d_repetition. repetition import get_factorial
 #from src.homework.d_repetition.repetition import sum_odd_numbers
 
 class Test_Config(unittest.TestCase):
