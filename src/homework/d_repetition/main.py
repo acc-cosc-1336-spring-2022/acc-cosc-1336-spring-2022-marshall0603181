cf5bc1a2333c154d5e25b6e83d9e4c11cdc41e4f
marshallb <marshall.brown2@g.austincc.edu>
2022-02-23 20:21:21 -0600

    everything works!

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,21 +1,27 @@
-print('Homework 3 Menu')
-choice = int(input('\n(1) Factorial \n(2) Sum odd numbers \n(3) Exit \n\n '))
 import repetition
 # choice has to do with 'Homework 3 menu'.
-if choice == 1:
-    num = int(input('enter number: '))
-    if num > 0 and num < 10:
-        print(repetition.get_factorial(num))
-elif choice == 2:
-    num = int(input('enter a number: '))
-    print(repetition.sum_odd_numbers(num))
-elif choice == 3:
-    exit('Exiting...')
-else:
-    print('Error')
+choice2 = 'y'
+while choice2 == 'y':
+    print('Homework 3 Menu')
+    choice = int(input('\n(1) Factorial \n(2) Sum odd numbers \n(3) Exit \n\n '))
+    if choice == 1:
+        num = int(input('enter number greater than 0 and less than 10: '))
+        if num > 0 and num < 10:
+            print(repetition.get_factorial(num))
+        else:
+            print('Invalid entry')
+            num = int(input('enter number greater than 0 and less than 10: '))
+            print(repetition.get_factorial(num))
+    elif choice == 2:
+        num = int(input('enter a number: '))
+        print(repetition.sum_odd_numbers(num))
+    elif choice == 3:
+        exit('Exiting...')
+    else:
+        print('Error')
+    choice2 = str(input('Do you wish to continue? y or n: '))
 
 # choice2 has to do with the question 'Do you want to exit? y or n: '.
-choice2 = str(input('Do you wish to continue? y or n: '))
 if choice2 == 'n':
     exit('Exiting...')
 elif choice2 == 'y':
