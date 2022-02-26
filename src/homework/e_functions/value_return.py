ab21966841f39780b77f06dd328e4c6f6b53f048
marshallb <marshall.brown2@g.austincc.edu>
2022-02-26 12:33:05 -0600

    added functions

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,11 @@
-#
+def get_hour(epoch_seconds):
+    pass
+
+def get_minutes(epoch_seconds):
+    pass
+
+def get_seconds(epoch_seconds):
+    pass
+
+def time_from_utc(utc_offset, utc_zero):
+    return utc_zero % utc_offset
