22d21b518380e4b8cb9f9afc65ab421b9bbb582d
marshallb <marshall.brown2@g.austincc.edu>
2022-02-28 18:33:32 -0600

    commit 02/28/2022

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,3 +1,20 @@
+def get_time(hour, minutes, seconds, time_type, meridien='AM'):
+    if hour < 10:
+        hour = "0" + str(hour)
+
+    if minutes < 10:
+        minutes = "0" + str(minutes)
+
+    if seconds < 10:
+        seconds = "0" + str(seconds)
+
+    time = str(hour) + ":" + str(minutes) + ":" + str(seconds)
+
+    if time_type == 12:
+        time = time + " " + meridien
+
+    return  time
+
 def get_hour(epoch_seconds):
     pass
 
