ff25f65050928b09d88807a887e3af7541730576
marshallb <marshall.brown2@g.austincc.edu>
2022-03-01 20:47:12 -0600

    commit

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -2,4 +2,5 @@
 import value_return_functions
 
 val = 0 # global variable
-value_return_functions.main(val)
+# value_return_functions.main(val)
+print(value_return_functions.VAL3)
--- src/examples/e_functions/value_return_functions.py
+++ src/examples/e_functions/value_return_functions.py
@@ -11,3 +11,12 @@ def main(val0):
     val = 0
     local_variable(val0)
     return val
+
+VAL3 = 10
+def use_global():
+    #VAL3 = 5
+    #print(VAL3)
+    global VAL3
+    VAL3 = 100
+
+print('global call', VAL3)
