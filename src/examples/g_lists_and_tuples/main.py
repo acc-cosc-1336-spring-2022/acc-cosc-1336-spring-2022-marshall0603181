b02b53673bca52e8554ef10f09816724e29b3e6a
marshallb <marshall.brown2@g.austincc.edu>
2022-03-24 19:58:38 -0500

    commit 24.03.22

--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -20,8 +20,16 @@ add_more = "y"
 
 while(add_more) == "y":
     item1 = input("enter item ")
-    list1.append_item_to_list(item1, list1)
+    lists.append_item_to_list(item1, list1)
     add_more = input("Enter y to continue")
+
+for item in list1:
+    print(item)
+
+old_value = input('enter a value to modify')
+new_value = input('enter new value')
+indx_to_change = list1.index(old_value)
+list1[indx_to_change] = new_value
 
 for item in list1:
     print(item)
