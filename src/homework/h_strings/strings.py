53bac37f7830e0aa459a68885bb5da9d53e535c3
marshallb <marshall.brown2@g.austincc.edu>
2022-03-31 09:22:44 -0500

    commit

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,24 +1,43 @@
 from re import X
-dna1 = 0
-dna2 = 0
-dna = 0
+def reverse_string(string):
+    rstr1 = ""
+    indx = len(string)
+    while indx > 0:
+        rstr1 += string[ indx - 1]
+        indx = indx - 1
+    return rstr1
+
+def list_to_string(s):
+    str1 = ""
+    for  ltr in s:
+        str1 += ltr
+    return str1
 
 def get_hamming_distance(dna1, dna2):
-    pass
+    lt_dna1 = list(dna1)
+    lt_dna2 = list(dna2)
+    cnt = 0
+    for i in range(17):
+        if lt_dna1[i] != lt_dna2[i]:
+            cnt += 1
+    return cnt
 
 
 
 def get_dna_complement(dna):
-    i = str(input('enter dna: '))
-    a = 'A'
-    t = 'T'
-    c = 'C'
-    g = 'G'
-    if a == a:
-        return t
-    if t == t:
-        return a
-    if c == c:
-        return g
-    if g == g:
-        return c
+    reverse = reverse_string(dna)
+    n_rvrs = list(reverse)
+    for i in range(10):
+        if n_rvrs[i] == "T":
+            n_rvrs[i] = "A"
+        elif n_rvrs[i] == "A":
+            n_rvrs[i] = "T"
+        elif n_rvrs[i] == "G":
+            n_rvrs[i] = "C"
+        elif n_rvrs[i] == "C":
+            n_rvrs[i] = "G"
+            i += 1
+
+    n_rvrs1 = list_to_string(n_rvrs)
+
+    return n_rvrs1
