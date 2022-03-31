cc959c73fd9f68c3303454b15e9854bca25a300b
marshallb <marshall.brown2@g.austincc.edu>
2022-03-31 09:55:56 -0500

    comit

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -2,7 +2,7 @@ import strings
 dna1 = 0
 dna2 = 0
 dna = 0
-choice1 = 'y'
+choice1 = ''
 while choice1 != '3':
     choice1 = str(input("1-Hamming Distance \n 2-Dna Complement \n 3-Exit \n"))
     if choice1 == '1':
@@ -10,7 +10,7 @@ while choice1 != '3':
         dna2 = str(input('enter dna2 17 characters long: '))
         print(strings.get_hamming_distance(dna1, dna2))
     elif choice1 == '2':
-        dna = str(input('enter dna1 10 characters long: '))
+        dna = str(input('enter dna 10 characters long: '))
         print(strings.get_dna_complement(dna))
     elif choice1 == '3':
         exit("Exiting...")
