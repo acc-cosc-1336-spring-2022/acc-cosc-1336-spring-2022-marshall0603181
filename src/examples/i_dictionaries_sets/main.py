b22c1e94831e9aaecb7fa4a0d8de0439838938b6
marshallb <marshall.brown2@g.austincc.edu>
2022-03-31 20:27:51 -0500

    commit

--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,1 +1,39 @@
 #main program
+
+phonebook = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
+
+print(phonebook['Katie'])
+
+phonebook['Katie'] = '555-4444'
+#print(phonebook['katie'])
+
+if 'Katie' in phonebook:
+    print(phonebook['Katie'])
+else:
+    print('Katie is not a valid key')
+
+if 'Katie' not in phonebook:
+    print('Katie not in phonebook')
+else:
+    print(phonebook['Katie'])
+
+del phonebook['Katie']
+
+print(len(phonebook))
+
+mixed_up = {'abc':1, 999:'yada yada', (3,6,9):[3,6,9]}
+
+print(mixed_up[999])
+print(mixed_up[3,6,9])
+
+#loop over the keys
+for key in mixed_up.values():
+    print(key, mixed_up[key])
+
+print(phonebook.get('Chris'))
+
+for key, value in phonebook.items():
+    phonebook[key] = '555-9999'
+    print(key, value)
+
+print(phonebook)
