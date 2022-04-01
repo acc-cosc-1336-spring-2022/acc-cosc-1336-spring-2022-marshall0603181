22d404551b8559f3baf787954444f2912e9eb532
marshallb <marshall.brown2@g.austincc.edu>
2022-03-31 20:37:30 -0500

    commit

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,3 +1,8 @@
 def test_config():
     return True
 
+def add_friend(name, key, bday, bdaydictionary):
+    bdaydictionary[name] = bday
+
+def update_friend(name, bday, bdaydictionary):
+    bdaydictionary[name] = bday
--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,49 +1,51 @@
-#main program
+# #main program
 
-phonebook = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
+# phonebook = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
 
-print(phonebook['Katie'])
+# print(phonebook['Katie'])
 
-phonebook['Katie'] = '555-4444'
-#print(phonebook['katie'])
+# phonebook['Katie'] = '555-4444'
+# #print(phonebook['katie'])
 
-if 'Katie' in phonebook:
-    print(phonebook['Katie'])
-else:
-    print('Katie is not a valid key')
+# if 'Katie' in phonebook:
+#     print(phonebook['Katie'])
+# else:
+#     print('Katie is not a valid key')
 
-if 'Katie' not in phonebook:
-    print('Katie not in phonebook')
-else:
-    print(phonebook['Katie'])
+# if 'Katie' not in phonebook:
+#     print('Katie not in phonebook')
+# else:
+#     print(phonebook['Katie'])
 
-del phonebook['Katie']
+# del phonebook['Katie']
 
-print(len(phonebook))
+# print(len(phonebook))
 
-mixed_up = {'abc':1, 999:'yada yada', (3,6,9):[3,6,9]}
+# mixed_up = {'abc':1, 999:'yada yada', (3,6,9):[3,6,9]}
 
-print(mixed_up[999])
-print(mixed_up[3,6,9])
+# print(mixed_up[999])
+# print(mixed_up[3,6,9])
 
-#loop over the keys
-#for key in mixed_up.values():
-    #print(key, mixed_up[key])
+# #loop over the keys
+# #for key in mixed_up.values():
+#     #print(key, mixed_up[key])
 
-print(phonebook.get('Chris'))
+# print(phonebook.get('Chris'))
 
-for key, value in phonebook.items():
-    phonebook[key] = '555-9999'
-    print(key, value)
+# for key, value in phonebook.items():
+#     phonebook[key] = '555-9999'
+#     print(key, value)
 
-print(phonebook)
+# print(phonebook)
 
-for key in phonebook.keys():
-    print(key)
+# for key in phonebook.keys():
+#     print(key)
 
-for v in phonebook.values():
-    print(v)
+# for v in phonebook.values():
+#     print(v)
 
-k,v = phonebook.popitem()
+# k,v = phonebook.popitem()
 
-print(k,v)
+# print(k,v)
+
+# print(phonebook)
