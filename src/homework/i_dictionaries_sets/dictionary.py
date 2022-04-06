ed9c6f4164c63968e1eb1a6f47e6eb772d683b99
marshallb <marshall.brown2@g.austincc.edu>
2022-04-06 16:28:08 -0500

    commit

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,20 @@
-#
+
+
+def get_p_distance(list1, list2):
+    indx = 0
+    difference = 0
+    while indx < len(list1):
+        if list1[indx] != list2[indx]:
+            difference += 1
+        indx += 1
+    p_distance = difference / len(list1)
+    return p_distance
+
+def get_p_distance_matrix(list1):
+    p_distance_matrix = []
+    for row_list in list1:
+        row = []
+        for column_list in list1:
+            row.append(get_p_distance(row_list, column_list))
+        p_distance_matrix.append(row)
+    return p_distance_matrix
