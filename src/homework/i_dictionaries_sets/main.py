28e651c936481eef66110b94b1d78608cc9a4198
marshallb <marshall.brown2@g.austincc.edu>
2022-04-07 21:58:51 -0500

    commit

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -6,7 +6,7 @@ choice = ""
 while choice != "2":
     choice = str(input("(1) Get p distance matrix \n(2) Exit \nEnter choice here: "))
     if choice == "1":
-        list1 = str(input("Please enter list: "))
+        list1 = list(input("Please enter list: "))
         print(dictionary.get_p_distance_matrix(list1))
     elif choice == "2":
         exit("Exiting...")
