3bcb0635d53af435b5108e90d45f70e77d085e70
marshallb <marshall.brown2@g.austincc.edu>
2022-04-14 19:29:24 -0500

    commit

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -18,4 +18,23 @@ def get_p_distance_matrix(list1):
         p_distance_matrix.append(row)
     return p_distance_matrix
 
-# Dave also helped me with this via GroupMe.
+# Dave also helped me with this via GroupMe
+
+def add_inventory(widgets):
+    quantity = 0
+    w = 'widget_name'
+    widgets = {}
+    widg_dict = {'widget_name':30}
+    if widgets[w] not in widgets:
+        widgets.update(widg_dict)
+        quantity += 1
+    else:
+        print('Error')
+
+def remove_inventory_widget(widget_name):
+    dictionary1 = {'widget1':30, 'widget2':40}
+    dictionary_len = len(dictionary1)
+    if dictionary1['widget1'] in dictionary1:
+        dictionary1.pop('widget1')
+    else:
+        return 'item not found'
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,6 +1,10 @@
+from audioop import add
+from os import remove
 import unittest
 from src.homework.i_dictionaries_sets.dictionary import get_p_distance_matrix
 from src.homework.i_dictionaries_sets.dictionary import get_p_distance
+from src.homework.i_dictionaries_sets.dictionary import add_inventory
+from src.homework.i_dictionaries_sets.dictionary import remove_inventory_widget
 
 class Test_Config(unittest.TestCase):
     def test_get_p_distance(self):
@@ -20,3 +24,13 @@ class Test_Config(unittest.TestCase):
                                 [0.1, 0.4, 0.0, 0.2],
                                 [0.1, 0.3, 0.2, 0.0]    ]
         self.assertEqual(p_distance_matrix, get_p_distance_matrix(dataset))
+
+    def test_add_inventory(self):
+        dictionary1 = dict({'widget_name':10})
+        self.assertEqual(10, add_inventory(dictionary1['widget_name']))
+        self.assertEqual(35, add_inventory({'Widget1':25}))
+        self.assertEqual(25, add_inventory({'Widget1':-10}))
+
+    def test_remove_inventory_widget(self):
+        self.assertEqual(1, remove_inventory_widget())
+        self.assertEqual(True, remove_inventory_widget())
