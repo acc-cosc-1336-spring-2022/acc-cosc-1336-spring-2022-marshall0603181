38d529b9d8e643ec12e0031ec16b7c8177f10ac2
marshallb <marshall.brown2@g.austincc.edu>
2022-04-16 09:26:35 -0500

    commit

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,40 +1,34 @@
 
-def get_p_distance(list1, list2):
-    i = 0
-    _diff_ = 0
-    while i < len(list1):
-        if list1[i] != list2[i]:
-            _diff_ += 1
-        i += 1
-    p_distance = _diff_ / len(list1)
-    return p_distance
+# def get_p_distance(list1, list2):
+#     i = 0
+#     _diff_ = 0
+#     while i < len(list1):
+#         if list1[i] != list2[i]:
+#             _diff_ += 1
+#         i += 1
+#     p_distance = _diff_ / len(list1)
+#     return p_distance
 
-def get_p_distance_matrix(list1):
-    p_distance_matrix = []
-    for row_list in list1:
-        row = []
-        for column_list in list1:
-            row.append(get_p_distance(row_list,column_list))
-        p_distance_matrix.append(row)
-    return p_distance_matrix
+# def get_p_distance_matrix(list1):
+#     p_distance_matrix = []
+#     for row_list in list1:
+#         row = []
+#         for column_list in list1:
+#             row.append(get_p_distance(row_list,column_list))
+#         p_distance_matrix.append(row)
+#     return p_distance_matrix
 
 # Dave also helped me with this via GroupMe
 
-def add_inventory(widgets):
-    quantity = 0
-    w = 'widget_name'
-    widgets = {}
-    widg_dict = {'widget_name':30}
-    if widgets[w] not in widgets:
-        widgets.update(widg_dict)
-        quantity += 1
+def add_inventory(widgets, widget_name, widget_quantity):
+    if widget_name not in widgets:
+        widgets[widget_name] = widget_quantity
     else:
-        print('Error')
+        widgets[widget_name] += widget_quantity
 
-def remove_inventory_widget(widget_name):
-    dictionary1 = {'widget1':30, 'widget2':40}
-    dictionary_len = len(dictionary1)
-    if dictionary1['widget1'] in dictionary1:
-        dictionary1.pop('widget1')
+def remove_inventory_widget(widgets, widget_name):
+    if widget_name in widgets:
+        del widgets[widget_name]
+        print('Record deleted')
     else:
-        return 'item not found'
+        print('Item not found')
--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,36 +1,60 @@
 import dictionary
 
-choice1 = "y"
-while choice1 == "y":
-    choice1 = str(input("Homework 6 Menu\n(1) Get p distance matrix\n(2) Exit\nEnter 1 or 2: \n"))
+# choice1 = "y"
+# while choice1 == "y":
+#     choice1 = str(input("Homework 6 Menu\n(1) Get p distance matrix\n(2) Exit\nEnter 1 or 2: \n"))
 
-    if choice1 == "1": # the choice for the get_p_distance matrix function
-        print("Enter two DNA strings of 10 characters or less. \nYou must only enter the letters of the DNA strings with no spaces between them.\n")
-        list1 = []
-        continue_ = "y"
-        while continue_ == "y":
-            dna_string = str(input("Enter a DNA string using the format: XXXXXX\n"))
-            dna_list = list(dna_string)
-            list1.append(dna_list)
-            print("Do you wish to add an additional DNA string?")
-            continue_ = str(input("Enter y for yes or n for no: "))
-        print("You have entered: ")
-        for string in list1:
-            print(string)
-        print("The following is the p distance matrix for the entered strings: ")
-        p_distance_matrix = dictionary.get_p_distance_matrix(list1)
-        for row in p_distance_matrix:
-            for item in row:
-                print(format(item, '8.3f'), end = '')
-            print('')
+#     if choice1 == "1": # the choice for the get_p_distance matrix function
+#         print("Enter two DNA strings of 10 characters or less. \nYou must only enter the letters of the DNA strings with no spaces between them.\n")
+#         list1 = []
+#         continue_ = "y"
+#         while continue_ == "y":
+#             dna_string = str(input("Enter a DNA string using the format: XXXXXX\n"))
+#             dna_list = list(dna_string)
+#             list1.append(dna_list)
+#             print("Do you wish to add an additional DNA string?")
+#             continue_ = str(input("Enter y for yes or n for no: "))
+#         print("You have entered: ")
+#         for string in list1:
+#             print(string)
+#         print("The following is the p distance matrix for the entered strings: ")
+#         p_distance_matrix = dictionary.get_p_distance_matrix(list1)
+#         for row in p_distance_matrix:
+#             for item in row:
+#                 print(format(item, '8.3f'), end = '')
+#             print('')
 
-    elif choice1 == "2": # the choice to exit the program
-        print("You have chosen exit.\n")
+#     elif choice1 == "2": # the choice to exit the program
+#         print("You have chosen exit.\n")
+#         exit("Exiting...")
+
+#     else:
+#         print("Invalid entry...\nHomework 6 Menu\n(1) Get p distance matrix\n(2) Exit")
+#         choice1 = str(input("Invalid entry...\nHomework 6 Menu\n(1) Get p distance matrix\n(2) Exit\nEnter 1 or 2: \n"))
+#     choice1 = 'y'
+
+# Dave helped me with this via GroupMe.
+
+choice1 = 'y'
+while choice1 == 'y':
+    choice1 = str(input("Homework 7 Menu\n(1) Add or Update Item\n(2) Delete Item\n(3) Exit"))
+    if choice1 == "1":
+        print('Please add or update your widgets item.')
+        widget_name = str(input('Enter name of item: '))
+        widget_quantity = int(input('Enter quantity of item added to widgets (as a positive number) or subtracted from widgets (as a negative number): '))
+        widgets = {}
+        dictionary.add_inventory(widgets, widget_name, widget_quantity)
+
+    elif choice1 == 2:
+        widget_name = input("Enter name for the item to be deleted: ")
+        dictionary.remove_inventory_widget(widgets, widget_name)
+
+    elif choice1 == 3:
         exit("Exiting...")
 
     else:
-        print("Invalid entry...\nHomework 6 Menu\n(1) Get p distance matrix\n(2) Exit")
-        choice1 = str(input("Invalid entry...\nHomework 6 Menu\n(1) Get p distance matrix\n(2) Exit\nEnter 1 or 2: \n"))
-    choice1 = 'y'
+        print("invalid entry")
 
-# Dave helped me with this via GroupMe.
+    choice1 = str(input("Would you like to continue back to the menu? y for yes or n for no"))
+    if choice1 == 'n':
+        exit("Exiting...")
