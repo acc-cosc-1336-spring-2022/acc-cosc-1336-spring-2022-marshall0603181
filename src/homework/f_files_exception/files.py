3243d31618ff5e940c967e60c099aa85096eb9f0
marshallb <marshall.brown2@g.austincc.edu>
2022-04-20 23:22:09 -0500

    commit

--- src/homework/f_files_exception/files.py
+++ src/homework/f_files_exception/files.py
@@ -1,1 +1,20 @@
-#
+# def add_inventory_item(items, item_name, item_quantity):
+#     if item_name not in items:
+#         items[item_name] = item_quantity
+#     else:
+#         items[item_name] += item_quantity
+
+# def display_inventory_item(items, item_name):
+#     if item_name in items:
+#         print(items[item_name])
+#     else:
+#         print('Item not found')
+
+def write_to_file(file_name, file_write):
+    f = open('C:/Users/marsh/acc_files/test.txt', "w")
+    f.write(file_write)
+    f.close()
+
+def read_from_file(file_name):
+    f = open(file_name, "r")
+    print(f.read())
--- src/homework/f_files_exception/main.py
+++ src/homework/f_files_exception/main.py
@@ -1,1 +1,25 @@
-#
+import files
+
+
+items = {}
+choice1 = 'y'
+while choice1 == 'y':
+    choice2 = str(input("Homework 7 Menu\n(1) Write text into a file\n(2) Display text in file\n(3) Exit\n"))
+    if choice2 == "1":
+        file_name = str(input("Enter name of file: \n"))
+        file_write = str(input("What would you like to write into this file?\n"))
+        files.write_to_file(file_name, file_write)
+
+    elif choice2 == "2":
+        file_name = input("Enter file path (without quotes) of the file you wish to be displayed: \n")
+        files.read_from_file(file_name)
+
+    elif choice2 == "3":
+        exit("Exiting...")
+
+    else:
+        print("invalid entry")
+
+    choice3 = str(input("Would you like to continue back to the main menu? y for yes or n for no:\n"))
+    if choice3 == 'n':
+        exit("Exiting...")
