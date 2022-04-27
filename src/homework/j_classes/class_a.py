e64dc83b74b6b63413a8be776393ff4daa9de7af
marshallb <marshall.brown2@g.austincc.edu>
2022-04-26 19:33:24 -0500

    commit

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -3,3 +3,7 @@ class Die:
     def roll():
         roll_value = random.randint(1,7)
         return roll_value
+    def get_rolled_value(roll_value):
+        print("The rolled value is ", roll_value,".")
+        
+    
