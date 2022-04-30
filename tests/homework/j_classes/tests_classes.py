3cbfee7778d7d29e7f2532dad8705903398be4ea
marshallb <marshall.brown2@g.austincc.edu>
2022-04-30 02:03:53 -0500

    commit

--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -12,3 +12,9 @@ class Test_Config(unittest.TestCase):
 
     def test_roll3(self):
         self.assertIn(Die.roll(self), [1,2,3,4,5,6])
+
+    def test_roll4(self):
+        self.assertIn(Die.roll(self), [1,2,3,4,5,6])
+    
+    def test_roll5(self):
+        self.assertIn(Die.roll(self), [1,2,3,4,5,6])
