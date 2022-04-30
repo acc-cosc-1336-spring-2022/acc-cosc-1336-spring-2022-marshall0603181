d3369e069ad9627f1e2dbfb31f548b92138bb43c
marshallb <marshall.brown2@g.austincc.edu>
2022-04-30 01:10:36 -0500

    commit

---
+++ src/examples/j_classes/class_practice.py
@@ -0,0 +1,16 @@
+ 
+class Bottle:
+    def __init__(self, volume, type_):
+        self.volume = volume
+        self.type_ = type_
+
+    def pour(self):
+        print("Pouring...")
+
+    def fill(self):
+        print("Filling...")
+
+    def recycle(self):
+        print("Recycling...")
+    
+    
